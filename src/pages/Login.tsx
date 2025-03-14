219335d615caece42510a9a5176399db695968d6
mominulislamsharon <ilsamikchannel328250@gmail.com>
2025-03-15 03:55:49 +0600

    added store cookies login persist

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,57 @@
+import { Button } from "@/components/ui/button";
+import { useLoginMutation } from "@/redux/features/auth/authApi";
+import { setUser } from "@/redux/features/auth/authSlice";
+import { useAppDispatch } from "@/redux/hook";
+import { verifyToken } from "@/utils/verifyToken";
+import { useForm } from "react-hook-form";
+
 const Login = () => {
+  const dispatch = useAppDispatch();
+
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      userEmail: "mon2@gmail.com",
+      password: "mondf4d3f4d3fd",
+    },
+  });
+
+  const [login, { error }] = useLoginMutation();
+
+  const onSubmit = async (data) => {
+    const userInfo = {
+      email: data.userEmail,
+      password: data.password,
+    };
+
+    const res = await login(userInfo).unwrap();
+    const user = verifyToken(res.data.token);
+    dispatch(setUser({ user: user, token: res.data.token }));
+  };
+
   return (
-    <div>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div>
-        login
+        <label htmlFor="email">Email:</label>
+        <input
+          className="border-2 border-blue-600"
+          type="email"
+          id="email"
+          {...register("userEmail")}
+        />
       </div>
-    </div>
+      <div>
+        <label htmlFor="id">Password:</label>
+        <input
+          className="border-2"
+          type="text"
+          id="password"
+          {...register("password")}
+        />
+      </div>
+      <Button type="submit" className="bg-white text-black">
+        Login
+      </Button>
+    </form>
   );
 };
 
